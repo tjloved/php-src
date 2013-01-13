890668d8e7a3301ee8be27aab4c9c8d9830dd048
Nikita Popov <nikic@php.net>
2013-01-13 19:13:23 +0100

    Fix error messages I accidentially broke

--- Zend/zend_object_handlers.c
+++ Zend/zend_object_handlers.c
@@ -1369,7 +1369,7 @@ ZEND_API zend_function *zend_std_get_static_method(zend_class_entry *ce, const c
 			if (ce->__callstatic) {
 				fbc = zend_get_user_callstatic_function(ce, function_name_strval, function_name_strlen);
 			} else {
-				zend_error_noreturn(E_ERROR, "Call to method %s %s::%s() from context '%s'", zend_visibility_string(fbc->common.fn_flags), ZEND_FN_SCOPE_NAME(fbc), function_name_strval, EG(scope) ? EG(scope)->name : "");
+				zend_error_noreturn(E_ERROR, "Call to %s method %s::%s() from context '%s'", zend_visibility_string(fbc->common.fn_flags), ZEND_FN_SCOPE_NAME(fbc), function_name_strval, EG(scope) ? EG(scope)->name : "");
 			}
 		}
 	} else if ((fbc->common.fn_flags & ZEND_ACC_PROTECTED)) {
@@ -1379,7 +1379,7 @@ ZEND_API zend_function *zend_std_get_static_method(zend_class_entry *ce, const c
 			if (ce->__callstatic) {
 				fbc = zend_get_user_callstatic_function(ce, function_name_strval, function_name_strlen);
 			} else {
-				zend_error_noreturn(E_ERROR, "Call to method %s %s::%s() from context '%s'", zend_visibility_string(fbc->common.fn_flags), ZEND_FN_SCOPE_NAME(fbc), function_name_strval, EG(scope) ? EG(scope)->name : "");
+				zend_error_noreturn(E_ERROR, "Call to %s method %s::%s() from context '%s'", zend_visibility_string(fbc->common.fn_flags), ZEND_FN_SCOPE_NAME(fbc), function_name_strval, EG(scope) ? EG(scope)->name : "");
 			}
 		}
 	}
