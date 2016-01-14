27db9724177bb89cb7410b8546337da16e5690bb
Nikita Popov <nikic@php.net>
2016-01-14 18:39:44 +0100

    Improve display of statistics

--- ext/opcache/Optimizer/statistics.c
+++ ext/opcache/Optimizer/statistics.c
@@ -4,7 +4,11 @@
 
 zend_optimizer_statistics optimizer_statistics;
 
-#define PRINT_STAT(name) fprintf(stderr, #name ": %" PRIu32 "\n", OPT_STAT(name))
+#define PRINT_STAT(name) \
+	fprintf(stderr, "%-25s: %6" PRIu32 "\n", #name, OPT_STAT(name))
+#define PRINT_NORM_STAT(name, norm) \
+	fprintf(stderr, "%-25s: %6" PRIu32 "   (%5.2f%% " #norm ")\n", \
+		#name, OPT_STAT(name), (float) OPT_STAT(name) / OPT_STAT(norm) * 100);
 
 void zend_optimizer_statistics_startup() {}
 void zend_optimizer_statistics_shutdown() {
@@ -14,20 +18,23 @@ void zend_optimizer_statistics_shutdown() {
 
 	PRINT_STAT(instrs);
 	PRINT_STAT(cfg_blocks);
+
 	PRINT_STAT(ssa_vars);
-	PRINT_STAT(ssa_may_be_ref);
-	PRINT_STAT(ssa_may_be_any);
-	PRINT_STAT(ssa_may_be_refcounted);
+	PRINT_NORM_STAT(ssa_may_be_ref, ssa_vars);
+	PRINT_NORM_STAT(ssa_may_be_any, ssa_vars);
+	PRINT_NORM_STAT(ssa_may_be_refcounted, ssa_vars);
+
 	PRINT_STAT(cv_ssa_vars);
-	PRINT_STAT(cv_ssa_may_be_ref);
-	PRINT_STAT(cv_ssa_may_be_any);
-	PRINT_STAT(cv_ssa_may_be_refcounted);
-	PRINT_STAT(cv_ssa_may_be_undef);
-	PRINT_STAT(scp_const_vars);
-	PRINT_STAT(scp_dead_blocks);
-	PRINT_STAT(scp_dead_instrs);
-	PRINT_STAT(scp_semi_dead_instrs);
-	PRINT_STAT(dce_dead_instr);
+	PRINT_NORM_STAT(cv_ssa_may_be_ref, cv_ssa_vars);
+	PRINT_NORM_STAT(cv_ssa_may_be_any, cv_ssa_vars);
+	PRINT_NORM_STAT(cv_ssa_may_be_refcounted, cv_ssa_vars);
+	PRINT_NORM_STAT(cv_ssa_may_be_undef, cv_ssa_vars);
+
+	PRINT_NORM_STAT(scp_const_vars, ssa_vars);
+	PRINT_NORM_STAT(scp_dead_blocks, cfg_blocks);
+	PRINT_NORM_STAT(scp_dead_instrs, instrs);
+	PRINT_NORM_STAT(scp_semi_dead_instrs, instrs);
+	PRINT_NORM_STAT(dce_dead_instr, instrs);
 	PRINT_STAT(copy_contracted_assign);
 	PRINT_STAT(copy_qm_assign);
 	PRINT_STAT(type_spec_arithm);
