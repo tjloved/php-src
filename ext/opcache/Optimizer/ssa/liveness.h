e387d5689799a691bd17b14e17856a9d3002ef43
Nikita Popov <nikic@php.net>
2016-01-23 23:15:09 +0100

    Liveness check implementation

---
+++ ext/opcache/Optimizer/ssa/liveness.h
@@ -0,0 +1,17 @@
+#ifndef _LIVENESS_H
+#define _LIVENESS_H
+
+typedef struct _ssa_liveness {
+	zend_ssa *ssa;
+	uint32_t block_set_len;
+	zend_bitset reduced_reachable;
+	zend_bitset targets;
+	zend_bitset sdom;
+	zend_bitset backedge_targets;
+} ssa_liveness;
+
+void ssa_liveness_precompute(zend_optimizer_ctx *opt_ctx, ssa_liveness *liveness, zend_ssa *ssa);
+zend_bool ssa_is_live_in_at_op(const ssa_liveness *liveness, int var_num, int op);
+
+#endif
+
