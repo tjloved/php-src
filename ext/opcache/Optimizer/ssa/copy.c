55625c80e7d071204c1dbf33df8735e71a7c8195
Nikita Popov <nikic@php.net>
2016-01-24 17:12:07 +0100

    Try out basic CSSA preserving copy propagation

--- ext/opcache/Optimizer/ssa/copy.c
+++ ext/opcache/Optimizer/ssa/copy.c
@@ -1,7 +1,70 @@
 #include "ZendAccelerator.h"
 #include "Optimizer/zend_optimizer_internal.h"
 #include "Optimizer/ssa_pass.h"
+#include "Optimizer/ssa/liveness.h"
 #include "Optimizer/statistics.h"
+
+int try_copy_propagation(ssa_opt_ctx *ctx, zend_op *opline, zend_ssa_op *ssa_op) {
+	zend_op_array *op_array = ctx->op_array;
+	zend_ssa *ssa = ctx->ssa;
+	zend_ssa_var *lhs_var = &ssa->vars[ssa_op->op1_def];
+	zend_ssa_var *rhs_var = &ssa->vars[ssa_op->op2_use];
+	int use;
+	//zend_ssa_phi *phi;
+	if (lhs_var->phi_use_chain || rhs_var->phi_use_chain) {
+		// TODO
+		return FAILURE;
+	}
+
+	/* Check if LHS is written is */
+	FOREACH_USE(lhs_var, use) {
+		zend_op *use_opline = &op_array->opcodes[use];
+		zend_ssa_op *use_op = &ssa->ops[use];
+		if (use_opline->opcode == ZEND_BIND_LEXICAL) {
+			// TODO allow if same name?
+			return FAILURE;
+		}
+		if ((use_op->op1_use == use && use_op->op1_def >= 0)
+				|| (use_op->op2_use == use && use_op->op2_def >= 0)
+				|| (use_op->result_use == use && use_op->result_def >= 0)) {
+			// TODO Actually, can't we handle this via liveness as well?
+			return FAILURE;
+		}
+	} FOREACH_USE_END();
+
+	/* Check if there is an assignment to the RHS at which the LHS is live */
+	FOREACH_USE(rhs_var, use) {
+		zend_ssa_op *use_op = &ssa->ops[use];
+		if ((use_op->op1_use == use && use_op->op1_def >= 0)
+				|| (use_op->op2_use == use && use_op->op2_def >= 0)
+				|| (use_op->result_use == use && use_op->result_def >= 0)) {
+			if (ssa_is_live_in_at_op(ctx->liveness, ssa_op->op1_def, use)) {
+				return FAILURE;
+			}
+		}
+	} FOREACH_USE_END();
+
+	/* Rename CV operands in uses */
+	FOREACH_USE(lhs_var, use) {
+		zend_op *use_opline = &op_array->opcodes[use];
+		zend_ssa_op *use_op = &ssa->ops[use];
+		if (use_op->op1_use == use) {
+			COPY_NODE(use_opline->op1, opline->op2);
+		}
+		if (use_op->op2_use == use) {
+			COPY_NODE(use_opline->op2, opline->op2);
+		}
+		if (use_op->result_use == use) {
+			COPY_NODE(use_opline->result, opline->op2);
+		}
+	} FOREACH_USE_END();
+
+	/* Update SSA */
+	rename_var_uses(ssa, ssa_op->op1_def, ssa_op->op2_use);
+
+	OPT_STAT(copy_propagated)++;
+	return SUCCESS;
+}
 
 void ssa_optimize_copy(ssa_opt_ctx *ctx) {
 	zend_op_array *op_array = ctx->op_array;
@@ -16,6 +79,12 @@ void ssa_optimize_copy(ssa_opt_ctx *ctx) {
 			RETURN_VALUE_USED(opline)
 		) {
 			continue;
+		}
+
+		if (opline->op2_type == IS_CV) {
+			if (try_copy_propagation(ctx, opline, ssa_op) == SUCCESS) {
+				continue;
+			}
 		}
 
 		if (ssa->var_info[ssa_op->op1_use].type & (MAY_BE_REF|MAY_BE_REFCOUNTED)) {
