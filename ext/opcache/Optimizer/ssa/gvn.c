ad714a785c073c6710a3ae54fec745bc52d21e59
Nikita Popov <nikic@php.net>
2016-01-30 16:06:18 +0100

    GVN stub

---
+++ ext/opcache/Optimizer/ssa/gvn.c
@@ -0,0 +1,8 @@
+#include "ZendAccelerator.h"
+#include "Optimizer/zend_optimizer_internal.h"
+#include "Optimizer/ssa_pass.h"
+#include "Optimizer/statistics.h"
+
+void ssa_optimize_gvn(ssa_opt_ctx *ctx) {
+}
+
