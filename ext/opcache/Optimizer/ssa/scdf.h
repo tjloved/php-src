f2f542cc4f704e71a4cf309f269927baa8b70737
Nikita Popov <nikic@php.net>
2016-02-15 18:40:57 +0100

    Refactor SCP on top of SCDF

--- ext/opcache/Optimizer/ssa/scdf.h
+++ ext/opcache/Optimizer/ssa/scdf.h
@@ -24,5 +24,22 @@ void scdf_solve(scdf_ctx *ctx, const char *name);
 void scdf_init(scdf_ctx *ctx, zend_op_array *op_array, zend_ssa *ssa);
 void scdf_free(scdf_ctx *ctx);
 
+static inline void scdf_add_to_worklist(scdf_ctx *ctx, int var) {
+	zend_bitset_incl(ctx->var_worklist, var);
+}
+
+static inline zend_bool scdf_is_edge_feasible(scdf_ctx *ctx, int from, int to) {
+	zend_basic_block *block = &ctx->ssa->cfg.blocks[from];
+	int suc;
+	if (block->successors[0] == to) {
+		suc = 0;
+	} else if (block->successors[1] == to) {
+		suc = 1;
+	} else {
+		ZEND_ASSERT(0);
+	}
+	return zend_bitset_in(ctx->feasible_edges, 2 * from + suc);
+}
+
 #endif
 
