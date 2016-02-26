0ee98d21ce1ce1786d4f3e7d9ab2cbbdbbe1abdd
Nikita Popov <nikic@php.net>
2016-02-26 15:36:02 +0100

    Separate compact_vars and ssa_destroy

--- ext/opcache/Optimizer/ssa_pass.h
+++ ext/opcache/Optimizer/ssa_pass.h
@@ -22,7 +22,8 @@ void ssa_optimize_gvn(ssa_opt_ctx *ctx);
 void ssa_optimize_cv_to_tmp(ssa_opt_ctx *ctx);
 void ssa_optimize_type_specialization(ssa_opt_ctx *ctx);
 void ssa_optimize_object_specialization(ssa_opt_ctx *ctx);
-void ssa_optimize_vars(ssa_opt_ctx *ctx);
+void ssa_optimize_compact_vars(ssa_opt_ctx *ctx);
+void ssa_optimize_destroy_ssa(ssa_opt_ctx *ctx);
 
 int ssa_verify_integrity(zend_ssa *ssa, const char *extra);
 
