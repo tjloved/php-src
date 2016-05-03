809b0eea94d64931b366600af6a2a4456465505c
Nikita Popov <nikic@php.net>
2016-05-03 23:38:24 +0200

    Properly DCE known COALESCE branches

--- ext/opcache/Optimizer/ssa/ssa_destruction.c
+++ ext/opcache/Optimizer/ssa/ssa_destruction.c
@@ -717,7 +717,6 @@ static inline void copy_instr(
 		case ZEND_JMPNZ_EX:
 		case ZEND_FE_RESET_R:
 		case ZEND_FE_RESET_RW:
-		case ZEND_NEW:
 		case ZEND_JMP_SET:
 		case ZEND_COALESCE:
 		case ZEND_ASSERT_CHECK:
