2294673440426de67533fb23c93378a42a765964
Nikita Popov <nikic@php.net>
2016-05-17 21:17:43 +0200

    SCDF: Use instr + phi-var worklists

--- ext/opcache/Optimizer/ssa/scdf.h
+++ ext/opcache/Optimizer/ssa/scdf.h
@@ -5,11 +5,15 @@ typedef struct _scdf_ctx {
 	void *ctx;
 	const zend_op_array *op_array;
 	zend_ssa *ssa;
-	zend_bitset var_worklist;
+	zend_bitset instr_worklist;
+	/* Represent phi-instructions through the defining var */
+	zend_bitset phi_var_worklist;
 	zend_bitset block_worklist;
 	zend_bitset executable_blocks;
-	zend_bitset feasible_edges; /* Encoding: 2 bits per block, one for each successor */
-	uint32_t var_worklist_len;
+	/* Edge encoding: 2 bits per block, one for each successor */
+	zend_bitset feasible_edges;
+	uint32_t instr_worklist_len;
+	uint32_t phi_var_worklist_len;
 	uint32_t block_worklist_len;
 
 	struct {
@@ -27,8 +31,17 @@ void scdf_init(scdf_ctx *scdf, const zend_op_array *op_array, zend_ssa *ssa, voi
 void scdf_solve(scdf_ctx *scdf, const char *name);
 void scdf_free(scdf_ctx *scdf);
 
-static inline void scdf_add_to_worklist(scdf_ctx *scdf, int var) {
-	zend_bitset_incl(scdf->var_worklist, var);
+static inline void scdf_add_to_worklist(scdf_ctx *scdf, int var_num) {
+	zend_ssa *ssa = scdf->ssa;
+	zend_ssa_var *var = &ssa->vars[var_num];
+	int use;
+	zend_ssa_phi *phi;
+	FOREACH_USE(var, use) {
+		zend_bitset_incl(scdf->instr_worklist, use);
+	} FOREACH_USE_END();
+	FOREACH_PHI_USE(var, phi) {
+		zend_bitset_incl(scdf->phi_var_worklist, phi->ssa_var);
+	} FOREACH_PHI_USE_END();
 }
 
 static inline zend_bool scdf_is_edge_feasible(scdf_ctx *scdf, int from, int to) {
